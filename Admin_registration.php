98730bc84522e528a45af5abea8e90d51561c6d3
Maricel - NHTBilling <Maricel - NHTBilling@DESKTOP-Q0SIVRV>
2024-10-24 06:10:29 +0800

    file path for css

--- Admin_registration.php
+++ Admin_registration.php
@@ -49,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="css/Admin_stylesheet.css">
+  <link rel="stylesheet" href="css/LoginForms.css"> <!-- Same CSS file -->
 </head>
 <body style="background-color:linear-gradient(to right, rgb(182, 244, 146), rgb(51, 139, 147));" class="abody">
 <section class="vh-100" >
@@ -82,8 +82,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                   </div>
 
                   <button name="login" data-mdb-button-init data-mdb-ripple-init class="btn btn-green btn-lg btn-block" type="submit">
-  Login
-</button>
+                Login
+              </button>
                   <a href="index.html" class="btn btn-lg btn-block btn-orange">Back to Web</a>
 
 
