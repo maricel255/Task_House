08c50e925f01132fbf305b0bf34256509b38dace
maricel255 <mariceldebebar@gmail.com>
2024-10-24 18:35:31 +0800

    to sample values for intacc and fa acc

--- Admin_users (1).sql
+++ Admin_users (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2024 at 12:24 AM
+-- Generation Time: Oct 24, 2024 at 12:34 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -28,17 +28,26 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `facacc` (
-  `faciID` varchar(5) NOT NULL,
-  `faciPass` varchar(5) NOT NULL
+  `faciID` varchar(10) NOT NULL,
+  `faciPass` varchar(10) NOT NULL,
+  `adminID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `facacc`
 --
 
-INSERT INTO `facacc` (`faciID`, `faciPass`) VALUES
-('67896', '67896'),
-('inter', 'inter');
+INSERT INTO `facacc` (`faciID`, `faciPass`, `adminID`) VALUES
+('bshmf', 'bshmf', 2),
+('cotfa', '12345', 1),
+('facot', 'facot', 1),
+('fahm1', 'fahm1', 2),
+('fahm2', 'fahm2', 2),
+('fahmt', 'fahmt', 2),
+('fahmt1', 'fahmt1', 2),
+('fahmt3', 'fahmt3', 2),
+('fahmt5', 'fahmt5', 2),
+('inter', 'inter', 0);
 
 -- --------------------------------------------------------
 
@@ -57,8 +66,13 @@ CREATE TABLE `intacc` (
 --
 
 INSERT INTO `intacc` (`internID`, `InternPass`, `adminID`) VALUES
-('123456', '123456', 2),
+('cot123', 'cot123', 1),
+('cot234', 'cot234', 1),
+('inhmt1', 'inhmt1', 2),
+('inhmt7', 'inhmt7', 2),
 ('intern1', 'intern1', 1),
+('inthm1', 'inthm1', 2),
+('inthm2', 'inthm2', 2),
 ('qwerty', 'qwerty', 2);
 
 -- --------------------------------------------------------
@@ -81,7 +95,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`adminID`, `Uname`, `Upass`, `admin_profile`, `Roles`, `Firstname`) VALUES
-(1, 'admin1', 'debebar', 0x31383034383437646663333764636230306562326466306264303834333965302e706e67, 'School Intern Coordinator', 'Debebar, Maricel'),
+(1, 'admin1', 'debebar', 0x63303563393832373033396366313539633132616539346436353033353233642e706e67, 'School Intern Coordinator', 'Debebar, Maricel'),
 (2, 'admin2', 'duterte', 0x33666236653330313639303131646266613033363537663163326632376565342e706e67, 'BSHM Intern Coordinator', 'Duterte, Sarah');
 
 --
@@ -92,7 +106,8 @@ INSERT INTO `users` (`adminID`, `Uname`, `Upass`, `admin_profile`, `Roles`, `Fir
 -- Indexes for table `facacc`
 --
 ALTER TABLE `facacc`
-  ADD PRIMARY KEY (`faciID`);
+  ADD PRIMARY KEY (`faciID`),
+  ADD UNIQUE KEY `faciID` (`faciID`);
 
 --
 -- Indexes for table `intacc`
