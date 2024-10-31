35338238087ddc1a9978431c5452105adf727a0c
Maricel - NHTBilling <Maricel - NHTBilling@DESKTOP-Q0SIVRV>
2024-11-01 06:17:39 +0800

    put into a css folder

--- intern_page.php
+++ intern_page.php
@@ -113,7 +113,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Expandable Sidebar with User Info</title>
-    <link rel="stylesheet" href="intern_styles.css">
+    <link rel="stylesheet" href="css/intern_styles.css">
 </head>
 <body>
     <div class="container">
