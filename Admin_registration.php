46dbe0715f132ca00a1d8684dd08a4bfa2db4e8f
maricel255 <mariceldebebar@gmail.com>
2024-11-23 09:27:22 +0800

    fix database connection

--- Admin_registration.php
+++ Admin_registration.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 session_start();
-require('./db_taskHouse/Admin_connection.php');
+require('db_Taskhouse/Admin_connection.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $Uname = $_POST['Uname'];
