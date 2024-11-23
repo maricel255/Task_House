31869587509123ae9d56f4b9c73b0a0a8af25bf5
maricel255 <mariceldebebar@gmail.com>
2024-11-23 09:30:26 +0800

    hide error debugger and fix the database connection path

--- Admin_Admin_1.php
+++ Admin_Admin_1.php
@@ -1,9 +1,9 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 session_start(); // Start the session
-require('db_taskHouse/Admin_connection.php');
+require('db_Taskhouse/Admin_connection.php');
 
 // Fetch the username from the session
 $Uname = $_SESSION['Uname'];
--- Admin_registration.php
+++ Admin_registration.php
@@ -1,8 +1,8 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-session_start();
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+// session_start();
 require('db_Taskhouse/Admin_connection.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
