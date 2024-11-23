bdf7d27b6931281817f3fbb9687d9dc3a8a27f41
maricel255 <mariceldebebar@gmail.com>
2024-11-23 09:53:54 +0800

    fix database path for css

--- intern_log.php
+++ intern_log.php
@@ -59,7 +59,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="css/LoginForms.css"> <!-- Same CSS file -->
+  <link rel="stylesheet" href="css/loginForms.css"> <!-- Same CSS file -->
 </head>
 <body style="background-color:linear-gradient(to right, rgb(182, 244, 146), rgb(51, 139, 147));" class="abody">
 <section class="vh-100">
