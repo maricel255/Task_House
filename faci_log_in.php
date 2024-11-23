259a328f99ac550bf0ac830fbf28e6664e011a0e
maricel255 <mariceldebebar@gmail.com>
2024-11-24 06:21:45 +0800

    path for db connect and css

--- faci_log_in.php
+++ faci_log_in.php
@@ -1,11 +1,11 @@
 <?php
-require('db_taskHouse/Admin_connection.php');
-
-session_start(); // Start the session
-
-// Enable error reporting for debugging
-error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start(); // Start the session
+require('db_Taskhouse/Admin_connection.php');
+
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $faciID = $_POST['Uname'];
@@ -50,7 +50,7 @@ $conn = null; // Close the connection
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="css/LoginForms.css"> <!-- Same CSS file -->
+  <link rel="stylesheet" href="css/loginForms.css"> <!-- Same CSS file -->
 </head>
 <body style="background-color:linear-gradient(to right, rgb(182, 244, 146), rgb(51, 139, 147));" class="abody">
 <section class="vh-100">
