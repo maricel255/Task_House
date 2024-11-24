9cc9c024777dd8747ede5b539289d4cac38bdb35
maricel255 <mariceldebebar@gmail.com>
2024-11-24 15:10:24 +0800

    fixing redirection

--- faci_facilitator.php
+++ faci_facilitator.php
@@ -1,7 +1,10 @@
 <?php
-require('db_taskHouse/Admin_connection.php');
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start(); // Start the session
+require('db_Taskhouse/Admin_connection.php');
 
 // Check if 'Uname' (faciID) is set in the session
 if (isset($_SESSION['Uname'])) {
