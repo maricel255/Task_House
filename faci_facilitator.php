7bcec1448d00dd755d6706e5906be4e416503f40
maricel255 <mariceldebebar@gmail.com>
2024-11-29 02:00:27 +0800

    fixing uploading image

--- faci_facilitator.php
+++ faci_facilitator.php
@@ -539,18 +539,17 @@ try {
         }
     ?>" alt="Profile Preview">
     
-    <form class="image-upload-form" action="" method="POST" enctype="multipart/form-data">
-    <div class="file-upload-container">
-        <input type="file" name="faci_image" accept="image/*" class="file-upload-input" 
-               onchange="updateFileName(this)">
-        <button type="button" class="file-upload-button">
-            <i class="fas fa-cloud-upload-alt"></i>
-            Choose Image
-        </button>
-        <div class="selected-file-name"></div>
+    <div class="buttons-container">
+        <form class="image-upload-form" action="" method="POST" enctype="multipart/form-data">
+            <div class="file-upload-container">
+                <input type="file" name="faci_image" accept="image/*" class="file-upload-input" onchange="this.form.submit()">
+                <button type="button" class="file-upload-button">
+                    <i class="fas fa-camera"></i>
+                    Change Photo
+                </button>
+            </div>
+        </form>
     </div>
-    <button type="submit" class="upload-submit-btn">Upload Image</button>
-</form>
 </div>
 
         <form id="credentialsForm" method="POST">
