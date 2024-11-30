1e493c35146514cc13f1eb98d076bfcee6d60415
maricel255 <mariceldebebar@gmail.com>
2024-12-01 01:54:46 +0800

    design gor log in forms

--- intern_log.php
+++ intern_log.php
@@ -43,46 +43,66 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intern</title>
+</head>
+<body>
+    
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
   <title>TH_INTERN LOG-IN</title>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="css/loginForms.css">
+  <link rel="stylesheet" href="css/loginForms.css"> <!-- Same CSS file -->
 </head>
-<body>
-<div class="login-container">
-  <div class="login-content">
-    <div class="card">
-      <div class="card-body p-4 p-lg-5 text-black">
-        <form method="POST" action="">
-          <span class="s-intern">INTERN'S</span>
-          <span class="s-TH">TASK HOUSE</span>
-          <h1 class="fw-normal mb-3 pb-3">INTERN LOG-IN</h1>
-          <div class="form-outline mb-4">
-            <input type="text" id="form2Example17" name="InternID" class="form-control form-control-lg" />
-            <label class="form-label" for="form2Example17">Intern ID</label>
+<body style="background-color:linear-gradient(to right, rgb(182, 244, 146), rgb(51, 139, 147));" class="abody">
+<section class="vh-100">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-10">
+        <div class="card" style="border-radius: 1rem;">
+          <div class="row g-0">
+            <div class="col-md-6 col-lg-5 d-none d-md-block">
+              <img src="image/th_logo.png" alt="Intern Login" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" width="500" height="600"/>
+            </div>
+            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+              <div class="card-body p-4 p-lg-5 text-black">
+                <form method="POST" action="">
+                  <span class="s-intern">INTERN'S</span>
+                  <span class="s-TH">TASK HOUSE</span>
+                  <h1 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">INTERN LOG-IN</h1>
+                  <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="text" id="form2Example17" name="InternID" class="form-control form-control-lg" />
+                    <label class="form-label" for="form2Example17">Intern ID</label>
+                  </div>
+                  <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="password" id="form2Example27" name="InternPass" class="form-control form-control-lg" />
+                    <label class="form-label" for="form2Example27">Password</label>
+                  </div>
+                  <button name="login" data-mdb-button-init data-mdb-ripple-init class="btn btn-green btn-lg btn-block" type="submit">
+                Login
+              </button>
+                                <a href="index.html" class="btn btn-lg btn-block btn-orange">Back to Web</a>
+                </form> 
+              </div>
+            </div>
           </div>
-          <div class="form-outline mb-4">
-            <input type="password" id="form2Example27" name="InternPass" class="form-control form-control-lg" />
-            <label class="form-label" for="form2Example27">Password</label>
-          </div>
-          <button name="login" class="btn btn-green btn-lg btn-block" type="submit">Login</button>
-          <a href="index.html" class="btn btn-lg btn-block btn-orange">Back to Web</a>
-        </form>
+        </div>
       </div>
     </div>
   </div>
-  
-  <!-- Character image positioned on the left -->
-  <div class="character-image">
-    <img src="image/3d-casual-life-young-man-pointing-at-smartphone.png" alt="Character" />
-  </div>
-  
-  <!-- Logo at bottom right -->
-  <div class="logo-container">
-    <img src="image/maninlogo__1_-removebg-preview.png" alt="Logo" class="document-logo"/>
-  </div>
-</div>
+</section>
+<script src="js/Intern_script.js"></script>
+ <!-- Logo positioned at bottom right -->
+ <div class="logo-container">
+          <img src="image/maninlogo__1_-removebg-preview.png" alt="Logo" class="document-logo"/>
+        </div>
+
 </body>
 </html>
