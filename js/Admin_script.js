5e5cebbd1b989e5dbf37f1c7db7e71e663289363
Kyle <christiankyleortalla@gmail.com>
2024-12-02 19:39:38 +0800

    back to normal

--- js/Admin_script.js
+++ js/Admin_script.js
@@ -260,13 +260,3 @@ function autoUploadImage(input) {
         });
     }
 }
-document.addEventListener('DOMContentLoaded', function() {
-    var form = document.querySelector('form');
-    if (form) {
-        form.onsubmit = function(e) {
-            e.preventDefault();
-            window.location.href = 'Admin_Admin_1.php';
-            return false;
-        };
-    }
-});
