d83ca1177d9643a70e8474a7884201c666924812
Kyle <christiankyleortalla@gmail.com>
2024-12-05 01:17:16 +0800

    fixing the error message when reload

--- Admin_Admin_1.php
+++ Admin_Admin_1.php
@@ -5,11 +5,6 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require('db_Taskhouse/Admin_connection.php');
 
-if (isset($_SESSION['message'])) {
-    echo "<div class='alert alert-warning'>" . htmlspecialchars($_SESSION['message'], ENT_QUOTES, 'UTF-8') . "</div>";
-    unset($_SESSION['message']); // Clear the message after displaying
-}
-
 // Add this function
 function setMessage($message, $type = 'info') {
     $_SESSION['message'] = $message;
@@ -1232,8 +1227,8 @@ if (isset($_POST['intern_id'])) {
         } catch (PDOException $e) {
             // If an exception occurs, show a user-friendly error message
             if ($e->getCode() == 23000) { // Integrity constraint violation
-                
-                $_SESSION['message'] = 'Cannot delete this record because it is referenced by another record.';
+            
+                echo "<script>alert('Cannot delete this record because it is referenced by another record.');</script>";
             } else {
                 echo "<script>alert('An unexpected error occurred. Please try again later.');</script>";
             }
