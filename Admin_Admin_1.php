1ce1011185e70b880127d053bd6206d634a1e7a5
maricel255 <mariceldebebar@gmail.com>
2024-12-04 14:38:50 +0800

    adding delete button in intern profile tab

--- Admin_Admin_1.php
+++ Admin_Admin_1.php
@@ -904,7 +904,6 @@ $timeLogsCount = $stmt->fetchColumn();
     }
     // END KYLE
 
-    // Handle the deletion for intern profile
 // Handle the deletion for intern profile
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] === 'delete') {
     $internID = $_POST['internID'] ?? null; // Get the internID from the POST request
